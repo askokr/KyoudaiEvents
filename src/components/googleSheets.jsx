c236483845d6afc10dd63182e8526a05e4539d7b
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-14 17:01:37 +0300

    Proovime taas sheetsi juppi

--- src/components/googleSheets.jsx
+++ src/components/googleSheets.jsx
@@ -1,52 +1,52 @@
-// import React, { Component } from "react";
-// import ReactGoogleSheets from "react-google-sheets";
+import React, { Component } from "react";
+import ReactGoogleSheets from "react-google-sheets";
 
-// class DataComponent extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       sheetLoaded: false
-//     };
-//   }
-//   render() {
-//     return (
-//       <ReactGoogleSheets
-//         clientId="664132969101-6l50abd7lmqsbf57kac2r6rt9k27ui1c.apps.googleusercontent.com"
-//         apiKey="AIzaSyDJEye_MuHDLq_yogKA1EtnfJdBnTvO9pg "
-//         spreadsheetId="1syL5nLI6lmz4qoMtshYTdZjx_Q5l75elG9iPtcKKgvk"
-//         afterLoading={() => this.setState({ sheetLoaded: true })}
-//       >
-//         {this.state.sheetLoaded ? (
-//           <div>
-//             {/* Access Data */}
-//             {console.log(
-//               "Your sheet data : ",
-//               this.props.getSheetsData("Events storage")
-//             )}
-//             {/* Update Data */}
-//             <button
-//               onClick={() => {
-//                 this.props.updateCell(
-//                   "sheet02", // sheetName
-//                   "E", // column
-//                   13, // row
-//                   "Apple", // value
-//                   null, // successCallback
-//                   error => {
-//                     console.log("error", error);
-//                   } // errorCallback
-//                 );
-//               }}
-//             >
-//               update cell!
-//             </button>
-//           </div>
-//         ) : (
-//           "loading..."
-//         )}
-//       </ReactGoogleSheets>
-//     );
-//   }
-// }
+class DataComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sheetLoaded: false
+    };
+  }
+  render() {
+    return (
+      <ReactGoogleSheets
+        clientId="64126451358-mmmraa7mnlsjktmbptde0v3fe5p6ns2g.apps.googleusercontent.com"
+        apiKey="AIzaSyDJEye_MuHDLq_yogKA1EtnfJdBnTvO9pg "
+        spreadsheetId="1syL5nLI6lmz4qoMtshYTdZjx_Q5l75elG9iPtcKKgvk"
+        afterLoading={() => this.setState({ sheetLoaded: true })}
+      >
+        {this.state.sheetLoaded ? (
+          <div>
+            {/* Access Data */}
+            {console.log(
+              "Your sheet data : ",
+              this.props.getSheetsData("Events storage")
+            )}
+            {/* Update Data */}
+            <button
+              onClick={() => {
+                this.props.updateCell(
+                  "sheet01", // sheetName
+                  "A", // column
+                  13, // row
+                  "Apple", // value
+                  null, // successCallback
+                  error => {
+                    console.log("error", error);
+                  } // errorCallback
+                );
+              }}
+            >
+              update cell!
+            </button>
+          </div>
+        ) : (
+          "loading..."
+        )}
+      </ReactGoogleSheets>
+    );
+  }
+}
 
-// export default ReactGoogleSheets.connect(DataComponent);
+export default ReactGoogleSheets.connect(DataComponent);
