af56dc6312ec35c9725ebb8550e1e99359a49636
Asko Kriiska <asko.kriiska@gmail.com>
2018-10-14 22:53:51 +0300

    changed cookie write

--- src/components/googleLogin.jsx
+++ src/components/googleLogin.jsx
@@ -13,7 +13,7 @@ class Login extends React.Component {
     console.log({ googleId });
     console.log({ accessToken: id_token });
     //anything else you want to do(save to localStorage)...
-    document.cookie = id_token.accessToken;
+    document.cookie = id_token;
   }
 
   render() {
